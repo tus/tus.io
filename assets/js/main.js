d17b3084478f4edb9461dd24b5528cda6f331ac3
Felix Geisendörfer <felix@debuggable.com>
2013-04-06 15:26:22 +0200

    More stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,60 @@
 $(function() {
-  var $githubs = $('#githubs');
-  $.getJSON('https://api.github.com/orgs/tus/events?callback=?', function(data, textStatus, jqXHR) {
+  loadGithubs().done(function(data) {
+    var $githubs = $('#githubs');
     for (var i = 0; i < data.data.length; i++) {
       var item = data.data[i];
       var $li = $('<li/>').appendTo($githubs);
       $('<img/>').addClass('gravatar').attr('src', item.actor.avatar_url+'&s=64').appendTo($li);
+      $('<span/>').text(' ').appendTo($li);
       $('<a/>').attr('href', item.actor.url).text(item.actor.login).appendTo($li);
 
       switch (item.type) {
         case 'IssueCommentEvent':
-          console.log(item);
-          $('<span/>').text('commented on ').appendTo($li);
+          $('<span/>').text(' commented on ').appendTo($li);
           $('<a/>').attr('href', item.payload.comment.url).text(item.payload.issue.title).appendTo($li);
-          $('<span/>').text('in').appendTo($li);
-          console.log(item);
+          $('<span/>').text(' in ').appendTo($li);
           $('<a/>').attr('href', item.repo.url).text(item.repo.name).appendTo($li);
           break;
+        case 'PushEvent':
+          $('<span/>').text(' pushed ' + item.payload.commits.length + ' commits to ').appendTo($li);
+          $('<a/>').attr('href', item.repo.url).text(item.repo.name).appendTo($li);
+          break;
+        case 'CreateEvent':
+          // @TODO: Not tested for ref_type != 'branch'
+          $('<span/>').text(' created a new ' + item.payload.ref_type).appendTo($li);
+          $('<span/>').text(' in ').appendTo($li);
+          $('<a/>').attr('href', item.repo.url).text(item.repo.name).appendTo($li);
+          $('<span/>').text(': ').appendTo($li);
+          $('<span/>').text(item.payload.ref).appendTo($li);
+        default:
+          console.log(item.type, item);
+          break;
       }
+      $('<abbr/>').text(item.created_at).attr('title', item.created_at).addClass('timeago').appendTo($li);
     }
-  })
+    $('<span/>').text(' — ').appendTo($li);
+    $('abbr.timeago').timeago();
+  });
+
+
+  function loadGithubs() {
+    var d = $.Deferred();
+
+    var githubs = localStorage.getItem('githubs');
+    try {
+      githubs = JSON.parse(githubs);
+    } catch (err) {
+    }
+
+    if (githubs) {
+      d.resolve(githubs);
+      return d;
+    }
+
+    $.getJSON('https://api.github.com/orgs/tus/events?per_page=100&callback=?', function(data, textStatus, jqXHR) {
+      localStorage.setItem('githubs', JSON.stringify(data));
+      d.resolve(data)
+    });
+    return d;
+  }
 });
--- index.md
+++ index.md
@@ -1,7 +1,20 @@
 ---
 layout: default
-title: tus
+title: Uploads are fucking broken
 ---
+
+<div class="jumbotron">
+  <h1>{{ page.title }}</h1>
+  <p class="lead">
+    Making a good file uploader for your app is too fucking hard. Mobile
+    networks are super unreliable, devices and and browser APIs are all over
+    the place, and there is no simple protocol with client and server
+    implementations you can use.
+  </p>
+</div>
+
+<hr>
+
 
 ## On the Githubs
 
