2227964cd71c9736fc7dca77343690431eb8167d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2013-04-08 11:03:08 +0200

    protect against rate-limiter & high build times

--- assets/json/community.sh
+++ assets/json/community.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -exu
+
+if find members.json -mmin -60 |grep members.json; then
+  # To protect against rate-limiter & high build times
+  echo "Your github community json files where built in the last 60 minutes. Not rebuilding. "
+  exit 0
+fi
+
 curl -ks https://api.github.com/orgs/tus/members > members.json
 
 combined=""
 for type in "collaborators" "subscribers" "stargazers"; do
+  
   for repo in "tus.io" "tusd" "tus-jquery-client" "tus-ios-client" "tus-android-client" "tus-resumable-upload-protocol"; do
     current="$(curl -ks https://api.github.com/repos/tus/${repo}/${type} |tail -n +2 |sed '$d')"
     if [ $? -eq 0 ] && [ -n "${current}" ]; then
