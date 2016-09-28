ff8cc35e58fa45f4f6d11d1db3c606e09c8e3132
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-09-28 21:04:38 +0200

    Fix for macOS sierra users

--- _scripts/post-install.sh
+++ _scripts/post-install.sh
@@ -9,5 +9,6 @@ source "${__dir}/_b3bp.sh"
 echo "--> Installing dependencies.."
 bower install
 which bundle 2>/dev/null || sudo gem install bundler -v 1.10 -n /usr/local/bin
+bundle config build.libv8 --with-system-v8 || true
 bundle install --path vendor/bundle || bundle update vendor/bundle
 rsync -a --progress --delete ./node_modules/{tus-js-client,on-the-githubs} ./_assets/node_modules/
