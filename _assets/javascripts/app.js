39fc0a14138ce36afea2b1ed8ded939b8785bd5e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-11-30 12:26:20 +0100

    First swing

--- _assets/javascripts/app.js
+++
@@ -1,6 +0,0 @@
-//=require bower/jquery/jquery.js
-//=require bower/jquery-timeago/jquery.timeago.js
-//=require node_modules/on-the-githubs/js/jquery.on-the-githubs.js
-//=require node_modules/tus-js-client/dist/tus.js
-//=require javascripts/main.js
-//=require javascripts/upload-demo.js
---
+++ assets/app.js
@@ -0,0 +1,15 @@
+window.jQuery = window.$ = require('jquery')
+require('jquery-timeago/jquery.timeago.js')
+require('on-the-githubs/js/jquery.on-the-githubs.js')
+require('tus-js-client/dist/tus.js')
+
+require('javascripts/app.js')
+
+
+require('skeleton/css/normalize.css')
+require('skeleton/css/skeleton.css')
+require('animate.css/source/_base.css')
+require('animate.css/source/fading_entrances/fadeIn.css')
+require('animate.css/source/flippers/flipOutX.css')
+
+require('stylesheets/app.css.less')
