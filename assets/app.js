f519a1659bcd0e5033cb2cbc4b39a74f433ad9ce
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-12-03 13:57:15 +0100

    Accept HMR

--- assets/app.js
+++ assets/app.js
@@ -4,14 +4,21 @@ window.$ = $;
 
 require('jquery-timeago/jquery.timeago.js')
 require('on-the-githubs/js/jquery.on-the-githubs.js')
-
-require('javascripts/app.js')
-
+require('./javascripts/app.js')
 
 require('skeleton/css/normalize.css')
 require('skeleton/css/skeleton.css')
 require('animate.css/source/_base.css')
 require('animate.css/source/fading_entrances/fadeIn.css')
 require('animate.css/source/flippers/flipOutX.css')
+require('./stylesheets/app.css.less')
 
-require('stylesheets/app.css.less')
+// check if HMR is enabled
+if (module.hot) {
+  module.hot.accept('./javascripts/app.js', function () {
+    require('./javascripts/app.js');
+  });
+  module.hot.accept('./stylesheets/app.css.less', function () {
+    require('./stylesheets/app.css.less');
+  });
+}
