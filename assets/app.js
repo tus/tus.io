510eec5309bcc45ecb81a777996abdde42cb05f1
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-12-05 12:10:50 +0100

    Move all assets under entry

--- assets/app.js
+++ assets/app.js
@@ -2,15 +2,7 @@ var $ = require('jquery');
 window.jQuery = $;
 window.$ = $;
 
-require('jquery-timeago/jquery.timeago.js')
-require('on-the-githubs/js/jquery.on-the-githubs.js')
 require('./javascripts/app.js')
-
-require('skeleton/css/normalize.css')
-require('skeleton/css/skeleton.css')
-require('animate.css/source/_base.css')
-require('animate.css/source/fading_entrances/fadeIn.css')
-require('animate.css/source/flippers/flipOutX.css')
 require('./stylesheets/app.css.less')
 
 // check if HMR is enabled
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,2 +1,4 @@
+require('jquery-timeago/jquery.timeago.js')
+require('on-the-githubs/js/jquery.on-the-githubs.js')
 require('./main.js.coffee')
 require('./upload-demo.js')
