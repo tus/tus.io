6c95fb1d3170833e54a7ef88ec0c4b249dfe2af3
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-05-01 09:12:43 +0100

    Ignore yarn artifacts and start ESLint setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
