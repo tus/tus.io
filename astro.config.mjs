fef9608ef3df19c5ee41afe00a1920645259ff44
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-05-09 11:50:02 +0100

    Update remark config and a11y emoji's

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
 import sitemap from '@astrojs/sitemap'
+import a11yEmoji from '@fec/remark-a11y-emoji'
+import { defineConfig } from 'astro/config'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import remarkToc from 'remark-toc'
-
-import mdx from '@astrojs/mdx'
+import remarkEmoji from 'remark-emoji'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tus.io',
-  integrations: [
-    preact({
-      compat: true,
-    }),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [preact({ compat: true }), sitemap(), mdx()],
   build: {
     format: 'file',
   },
@@ -23,7 +20,11 @@ export default defineConfig({
   },
   markdown: {
     // See https://github.com/remarkjs/remark-toc
-    remarkPlugins: [[remarkToc, { tight: true, maxDepth: 2, ordered: true }]],
+    remarkPlugins: [
+      [remarkEmoji, { accessible: true, emoticon: true }],
+      [remarkToc, { tight: true, maxDepth: 2, ordered: true }],
+    ],
+    rehypePlugins: [rehypeHeadingIds, rehypeAutolinkHeadings],
     shikiConfig: {
       theme: 'github-light',
     },
