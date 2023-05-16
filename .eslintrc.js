254d1965d312b0f831d477e4a5bd005aa20ac379
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-05-16 11:20:33 +0100

    Don't lint Astro files with `eslint-plugin-react-*`

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,49 +30,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:astro/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint', 'astro', 'react', 'react-hooks'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    /**
-     * Preact / JSX rules
-     * From: https://github.com/preactjs/eslint-config-preact/blob/master/index.js
-     */
-    'react/no-deprecated': 'error',
-    'react/react-in-jsx-scope': 'off', // handled this automatically
-    'react/display-name': ['warn', { ignoreTranspilerName: false }],
-    'react/jsx-no-bind': [
-      1,
-      {
-        ignoreRefs: true,
-        allowFunctions: true,
-        allowArrowFunctions: true,
-      },
-    ],
-    'react/jsx-no-comment-textnodes': 'error',
-    'react/jsx-no-duplicate-props': 'error',
-    'react/jsx-no-target-blank': 'error',
-    'react/jsx-no-undef': 'error',
-    'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
-    'react/jsx-uses-react': 'error', // debatable
-    'react/jsx-uses-vars': 'error',
-    'react/jsx-key': ['error', { checkFragmentShorthand: true }],
-    'react/self-closing-comp': 'error',
-    'react/prefer-es6-class': 'error',
-    'react/prefer-stateless-function': 'warn',
-    'react/require-render-return': 'error',
-    'react/no-danger': 'warn',
-    // Legacy APIs not supported in Preact:
-    'react/no-did-mount-set-state': 'error',
-    'react/no-did-update-set-state': 'error',
-    'react/no-find-dom-node': 'error',
-    'react/no-is-mounted': 'error',
-    'react/no-string-refs': 'error',
-
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-
     '@typescript-eslint/consistent-type-exports': 'error',
     '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/no-import-type-side-effects': 'error',
@@ -85,7 +46,9 @@ module.exports = {
       },
     },
     {
+      extends: ['plugin:astro/recommended', 'prettier'],
       files: ['*.astro'],
+      plugins: ['astro'],
       // Allows Astro components to be parsed.
       parser: 'astro-eslint-parser',
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
@@ -96,6 +59,49 @@ module.exports = {
       },
       rules: {
         'react/self-closing-comp': 'off',
+      },
+    },
+    {
+      files: ['*.tsx'],
+      plugins: ['react', 'react-hooks'],
+      rules: {
+        /**
+         * Preact / JSX rules
+         * From: https://github.com/preactjs/eslint-config-preact/blob/master/index.js
+         */
+        'react/no-deprecated': 'error',
+        'react/react-in-jsx-scope': 'off', // handled this automatically
+        'react/display-name': ['warn', { ignoreTranspilerName: false }],
+        'react/jsx-no-bind': [
+          1,
+          {
+            ignoreRefs: true,
+            allowFunctions: true,
+            allowArrowFunctions: true,
+          },
+        ],
+        'react/jsx-no-comment-textnodes': 'error',
+        'react/jsx-no-duplicate-props': 'error',
+        'react/jsx-no-target-blank': 'error',
+        'react/jsx-no-undef': 'error',
+        'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+        'react/jsx-uses-react': 'error', // debatable
+        'react/jsx-uses-vars': 'error',
+        'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+        'react/self-closing-comp': 'error',
+        'react/prefer-es6-class': 'error',
+        'react/prefer-stateless-function': 'warn',
+        'react/require-render-return': 'error',
+        'react/no-danger': 'warn',
+        // Legacy APIs not supported in Preact:
+        'react/no-did-mount-set-state': 'error',
+        'react/no-did-update-set-state': 'error',
+        'react/no-find-dom-node': 'error',
+        'react/no-is-mounted': 'error',
+        'react/no-string-refs': 'error',
+
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
       },
     },
   ],
