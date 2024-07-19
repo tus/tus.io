b019716c4d5ce9c51863dc412789ae22f84e3494
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2024-07-19 10:16:21 +0200

    Add throttling for Octokit

--- src/lib/octokit.ts
+++ src/lib/octokit.ts
@@ -2,6 +2,7 @@ import { Octokit } from '@octokit/core'
 import { paginateRest } from '@octokit/plugin-paginate-rest'
 import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods'
 import { retry } from '@octokit/plugin-retry'
+import { throttling } from '@octokit/plugin-throttling'
 
 export const user = 'tus'
 
@@ -16,11 +17,35 @@ export const repos = [
   'tus-resumable-upload-protocol',
 ]
 
-const TusOctokit = Octokit.plugin(retry, restEndpointMethods, paginateRest)
+const TusOctokit = Octokit.plugin(
+  throttling,
+  retry,
+  restEndpointMethods,
+  paginateRest,
+)
 
 export const octokit = new TusOctokit({
   request: { fetch: fetch },
   auth: import.meta.env.GITHUB_TOKEN
     ? `token ${import.meta.env.GITHUB_TOKEN}`
     : undefined,
+  throttle: {
+    onRateLimit: (retryAfter, options, octokit, retryCount) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`,
+      )
+
+      if (retryCount < 1) {
+        // only retries once
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`)
+        return true
+      }
+    },
+    onSecondaryRateLimit: (retryAfter, options, octokit) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`,
+      )
+    },
+  },
 })
